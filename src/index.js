a0fea79d092b3c6b2ad976f1fdae2e5769311217
Julia Medvedeva <60058448+julmedvedeva@users.noreply.github.com>
2022-09-18 14:52:40 +0300

    Tuning webpack  (#1)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss'
+
+ReactDOM.render(
+  <h1>Hello from js</h1>,
+  document.querySelector(
+    '#fromjs'
+  )
+);
